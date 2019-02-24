14e086cc4ba26be11234d657a5b6e260709aae3f
Cheng Vue <csalvue@gmail.com>
2019-02-24 12:04:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function printBadges(badges) {
   }
   return badges;
 }
+
+function tailsNeverFails() {
+  let counter = 0;
+
+  while(Math.random() >= 0.5) {
+    counter++;
+  }
+
+  return `You got %{counter} tails in a row!`;
+}
